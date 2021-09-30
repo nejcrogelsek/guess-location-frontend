53ce0944ab71fc37d791e9211ff656570136e3cf
Nejc <nejcrogelsek0@gmail.com>
2021-09-30 22:25:53 +0200

    config eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { Loading } from './pages';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { Loading } from './pages'
+import reportWebVitals from './reportWebVitals'
 
-if (process.env.NODE_ENV === "development") {
-  require("dotenv").config();
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Suspense fallback={<Loading />}><App /></Suspense>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <Suspense fallback={<Loading />}><App /></Suspense>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
-reportWebVitals();
+reportWebVitals()
