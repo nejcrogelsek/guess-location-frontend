55a50522a621dbe5f53a5b322aa070eeb4091a0a
Nejc <nejcrogelsek0@gmail.com>
2021-10-07 19:31:34 +0200

    init swiper slider

--- src/components/Home/Slider.tsx
+++ src/components/Home/Slider.tsx
@@ -1,80 +1,27 @@
 import { FC } from 'react'
-import Slider from 'react-slick'
-import 'slick-carousel/slick/slick.css'
-import 'slick-carousel/slick/slick-theme.css'
 import Card from '../shared/Card'
 import locationStore from '../../stores/location.store'
 import { observer } from 'mobx-react-lite'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+// Import Swiper styles
+import 'swiper/css'
 
 const HomeSlider: FC = () => {
-	const settings = {
-		dots: false,
-		infinite: false,
-		speed: 500,
-		slidesToShow: 3,
-		slidesToScroll: 0,
-		autoplay: false,
-		centerMode: true,
-		autoplaySpeed: 2000,
-		cssEase: 'linear',
-		responsive: [
-			{
-				breakpoint: 9999,
-				settings: {
-					slidesToShow: 3,
-					slidesToScroll: 0,
-				},
-			},
-			{
-				breakpoint: 991,
-				settings: {
-					slidesToShow: 1.9,
-					slidesToScroll: 1,
-				},
-			},
-			{
-				breakpoint: 900,
-				settings: {
-					slidesToShow: 1.5,
-					slidesToScroll: 1,
-				},
-			},
-			{
-				breakpoint: 768,
-				settings: {
-					slidesToShow: 1.2,
-					slidesToScroll: 1,
-				},
-			},
-			{
-				breakpoint: 700,
-				settings: {
-					slidesToShow: 1,
-					slidesToScroll: 1,
-				},
-			},
-			{
-				breakpoint: 600,
-				settings: {
-					slidesToShow: 0.9,
-					slidesToScroll: 1,
-				},
-			},
-			{
-				breakpoint: 460,
-				settings: {
-					slidesToShow: 0.8,
-					slidesToScroll: 1,
-				},
-			},
-		],
-	}
 	return (
-		<Slider {...settings}>
-			{locationStore.recentLocations?.slice(0, 3).map((location) => (
-				<Card key={location.id} {...location} bottom='24px' />
-			))}
-		</Slider>
+		<>
+			<Swiper
+				spaceBetween={50}
+				slidesPerView={3}
+				onSlideChange={() => console.log('slide change')}
+				onSwiper={(swiper: any) => console.log(swiper)}>
+				{locationStore.recentLocations?.slice(0, 3).map((location) => (
+					<SwiperSlide key={location.id}>
+						<Card {...location} bottom='24px' />
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</>
 	)
 }
 
